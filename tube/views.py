309cb5b93fbde202afbc3ddf48d34deba5699bbf
lwj0712 <dldnjswot@naver.com>
2024-08-21 16:40:40 +0900

    tube 앱 views를 CBV를 사용하여 CRUD 작성

--- tube/views.py
+++ tube/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DeleteView,
+    UpdateView,
+    DetailView,
+    CreateView,
+)
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+
+post_list = PostListView.as_view()
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy("tube:post_list")
+    template_name = "tube/form.html"
+
+
+post_new = PostCreateView.as_view()
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+post_detail = PostDetailView.as_view()
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy("tube:post_list")
+    template_name = "tube/form.html"
+
+
+post_edit = PostUpdateView.as_view()
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy("tube:post_list")
+
+
+post_delete = PostDeleteView.as_view()
